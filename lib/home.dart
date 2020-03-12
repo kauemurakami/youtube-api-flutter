bd1be1edbbe610d83e1400b47a0da291e458b8d6
kaue murakami <kauetmurakami@gmail.com>
2020-03-12 15:10:56 -0300

    upload inicial do projeto, tela inicial

---
+++ lib/home.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  
+  int _indiceAtual = 0;
+  @override
+  Widget build(BuildContext context) {
+
+
+    List<Widget> telas = [
+      Text("Ínicio"),
+      Text("Em alta"),
+      Text("Inscrições"),
+      Text("Biblioteca"),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.grey,
+          opacity: 1,
+        ),
+        backgroundColor: Colors.white,
+        title: Image.asset(
+          "images/youtube.png",
+          width: 98,
+          height: 22,
+          ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.videocam), 
+            onPressed: (){
+              print("videocam");
+            }
+            ),
+            IconButton(
+            icon: Icon(Icons.search), 
+            onPressed: (){
+              print("search");
+            }
+            ),
+            IconButton(
+            icon: Icon(Icons.account_circle), 
+            onPressed: (){
+              print("account");
+            }
+            ),
+        ],
+      ),
+      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        type: BottomNavigationBarType.shifting,
+        currentIndex: _indiceAtual,
+        onTap: (index){
+          setState(() {
+            _indiceAtual = index;
+          });
+        },
+        fixedColor: Colors.red,
+        items: [
+          BottomNavigationBarItem(
+            //backgroundColor: Colors.greenAccent,
+            title: Text("Início"),
+            icon: Icon(Icons.home),
+            ),
+          BottomNavigationBarItem(
+            //backgroundColor: Colors.amber[400],
+            title: Text("Em alta"),
+            icon: Icon(Icons.whatshot)
+          ),
+          BottomNavigationBarItem(
+            //backgroundColor: Colors.lightGreenAccent,
+            title: Text("Inscrições"),
+            icon: Icon(Icons.subscriptions),
+          ),
+          BottomNavigationBarItem(
+            //backgroundColor: Colors.cyanAccent,
+            title: Text("Biblioteca"),
+            icon: Icon(Icons.folder),
+          ),
+        ]
+      ),
+    );
+  }
+}
